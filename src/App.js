d9a708431e3186ad3f561c45867bd0299521fdfa
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-31 15:36:22 -0600

    Complete the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import "./App.css";
+import { connect } from "react-redux";
+import { updateUser, getUsers } from "./actions/userActions";
 
-import './App.css';
-
-function App() {
+function App(props) {
+  const onUpdateUser = () => {
+   // props.dispatch(updateUser("New TommyXYZ"));
+   props.onUpdateUser('John')
+ 
+  };
+  console.log("App Comp=>", props);
   return (
     <div className="App">
-      <h1> App Component  works</h1>
+      <h1>App Component</h1>
+      <h2>{props.user}</h2>
+      <button onClick={onUpdateUser}>Change The Name</button>
+      <button onClick={()=>{props.onGetUsers();}}>Change The Name FROM API</button>
     </div>
   );
 }
+const mapStateToProps = (state, props) => ({
+  user: state.user,
+});
 
-export default App;
+const mapDispatchToProps = {
+  onUpdateUser: updateUser,
+  onGetUsers: getUsers
+};
+
+
+// const mergeProps = (propsFromState, propsFromDispatch, ownprops) => {
+//   console.log("propsFromState:",propsFromState);
+//   console.log("propsFromDispatch:",propsFromDispatch);
+//   console.log("ownprops:",ownprops);
+//   return {}
+// } 
+
+// export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
